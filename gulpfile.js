1bd878410ac6f8a97c91c3011531a7580a1847e2
Benjamin Horn <benjamin.horn@idean.com>
2015-05-22 15:10:58 +0300

    Rewrote script to use compass with gulp (replicate a lot of the behaviour on the drupal installation).

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,51 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
+var compass = require('gulp-compass');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
-// sass task
-gulp.task('sass', function () {
-	sass(
-		'./sass/main.scss', 
-		{ style: 'expanded' }
-	).pipe(autoprefixer({
-            browsers: ['last 4 versions'],
-            cascade: false
-    })).pipe(
-    	gulp.dest(
-    		'./css'
-    	)
-    );
-});
+// Compass task
+gulp.task(
+	'compass', 
+	function() {
+		gulp.src('./sass/**/*.scss')
+		.pipe(
+			compass(
+				{
+					config_file: './config.rb',
+					environment: 'production',
+					css: './css',
+					sass: './sass',
+					debug: false,
+					time: true
+				}
+			)
+		).on(
+			'error', 
+			function(error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		).pipe(
+			autoprefixer(
+				{
+					browsers: ['last 4 versions'],
+					cascade: false
+				}
+			)
+		).pipe(
+			gulp.dest(
+				'./css'
+			)
+		)
+	}
+);
 
 gulp.task('watch', function() {
 	// watch scss files
-	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./sass/**/*.scss', ['compass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['compass', 'watch']);
