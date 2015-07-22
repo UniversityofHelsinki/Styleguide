35f44735ac84df095f5dd43240fd4375b97bf231
Marko Rautajoki <marko.rautajoki@gofore.com>
2015-07-22 12:35:24 +0300

    Added styleguide building to gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
 var compass = require('gulp-compass');
-var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
 var browserSync = require('browser-sync').create();
+var shell = require('gulp-shell')
 
 // Compass task
 gulp.task(
 	'compass', 
 	function() {
-		gulp.src('./sass/**/*.scss')
+		return gulp.src('./sass/**/*.scss')
 		.pipe(
 			compass(
 				{
@@ -45,12 +45,18 @@ gulp.task(
 	}
 );
 
-gulp.task('serve', ['compass'], function() {
+gulp.task('build', ['compass'], function() {
+  return gulp.src('')
+    .pipe(shell(['node build'], {cwd : './styleguide'}));
+})
+
+gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: ""
   });
-  gulp.watch('./sass/**/*.scss', ['compass']);
-  gulp.watch("index.html").on('change', browserSync.reload);
+  gulp.watch('./sass/**/*.scss', ['build']);
+
+  gulp.watch(["index.html", "./templates/**/*.html"]).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['compass']);
+gulp.task('default', ['build']);
